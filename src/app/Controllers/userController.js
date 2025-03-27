0bbb124d183c3a00d2c602d9af7f91afd5de40a7
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-03-27 17:03:55 +0100

    Registration

--- src/app/Controllers/userController.js
+++ src/app/Controllers/userController.js
@@ -2,7 +2,25 @@ userModel = require("../Models/userModel");
 
 
 exports.register = (req,res) => {
+    const name = req.body.name.trim();
+    const password = req.body.password.trim();
+    const passwordCheck = req.body.passwordCheck.trim();
+    const aiCheck = req.body.aiCheck
 
+    console.log(req.body)
+
+    if(aiCheck != "on")
+        return res.redirect("/user/registerGet");
+    if(!name || !name)
+        return res.redirect("/user/registerGet");
+    if(password != passwordCheck)
+        return res.redirect("/user/registerGet");
+    if(!userModel.checkName(name))
+        return res.redirect("/user/registerGet");
+
+    userModel.addUser(name, password);
+
+    return res.redirect("/user/loginGet");
 }
 
 exports.login = (req,res) => {
--- src/app/Models/userModel.js
+++ src/app/Models/userModel.js
@@ -6,11 +6,25 @@ if (!db.has("next_id"))
     db.set("next_id",1);
 
 exports.checkName = (username) => {
+    const data = db.JSON()
+    delete data["next_id"]
 
+    for(let user in data)
+        if(username == user.name)
+            return false
+    return true
 }
 
 exports.addUser = (username, password) => {
+    const id = db.get("next_id");
+    let hashedPassword = bcrypt.hashSync(password)
 
+    db.set(id, {
+        id,
+        username,
+        hashedPassword
+    })
+    db.set("next_id", id+1)
 }
 
 exports.checkPassword = (id, password) => {
@@ -18,5 +32,4 @@ exports.checkPassword = (id, password) => {
 }
 
 exports.userFromName = (username) => {
-    
 }
