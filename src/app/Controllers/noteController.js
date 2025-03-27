8fa06bf905d97d57151def7af53b25e3f3d202e5
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-03-27 09:09:21 +0100

    Controller function setup

--- src/app/Controllers/noteController.js
+++ src/app/Controllers/noteController.js
@@ -1,1 +1,14 @@
 noteModel = require("../Models/noteModel");
+
+
+exports.add = (req,res) => {
+
+}
+
+exports.delete = (req,res) => {
+
+}
+
+exports.setImportance = (req, res) => {
+    
+}
--- src/app/Controllers/userController.js
+++ src/app/Controllers/userController.js
@@ -1,1 +1,26 @@
-userModel = require("../Models/userModel"); 
+userModel = require("../Models/userModel");
+
+
+exports.register = (req,res) => {
+
+}
+
+exports.login = (req,res) => {
+
+}
+
+exports.logout = (req,res) => {
+    
+}
+
+exports.profile = (req,res) => {
+
+}
+
+exports.registerGet = (req,res) => {
+
+}
+
+exports.loginGet = (req,res) => {
+
+}
